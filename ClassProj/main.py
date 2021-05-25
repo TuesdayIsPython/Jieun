a20042e227129409f37cfdbf61fce2c3700a4195
hwangji-dev <melon2228@naver.com>
2021-05-26 00:05:30 +0900

    [upload]

---
+++ ClassProj/main.py
@@ -0,0 +1,33 @@
+'''
+class Musician:
+    title = "Rockstar"
+
+    def explanation(self):
+        print("I am a {}!".format(self.title))
+
+
+drummer = Musician()
+drummer.explanation()
+'''
+
+'''
+class fourCal:
+    def add(self, first, second):
+        result = first + second
+        return result
+    def sub(self, first, second):
+        result = first - second
+        return result
+    def mul(self, first, second):
+        result = first * second
+        return result
+    def div(self, first, second):
+        result = first / second
+        return result
+
+a = fourCal()
+print(a.div(4,2))
+'''
+
+
+print(divmod(7,3))
